9fc506eaa4feb4fb35071b4ae3ea6563a0b427a1
Jared Ledvina <jared@techsmix.net>
2018-06-27 07:33:50 -0400

    Unfuck sensu-go Inspec tests

--- molecule/shared/tests/test_default.rb
+++ molecule/shared/tests/test_default.rb
@@ -1,52 +1,128 @@
-# Verify that redis, rabbitmq-server, sensu-{api,client}, and Uchiwa
-# are all listening as expected
+# Debian Specific Things
+if os.debian?
+  describe package('curl') do
+    it { should be_installed }
+  end
 
-# Redis
-describe port(6379) do
-  it { should be_listening }
-  its('protocols') { should include 'tcp' }
-  its('addresses') { should be_in ['0.0.0.0', '::'] }
+  describe package('gnupg') do
+    it { should be_installed }
+  end
+
+  describe package('debian-archive-keyring') do
+    it { should be_installed }
+  end
+
+  describe package('apt-transport-https') do
+    it { should be_installed }
+  end
 end
 
-# RabbitMQ Server
-describe port(5671) do
-  it { should be_listening }
-  its('protocols') { should include 'tcp' }
-  its('addresses') { should be_in ['0.0.0.0', '::'] }
+if os.name == 'debian'
+  describe apt('https://packagecloud.io/sensu/prerelease/debian/') do
+    it { should exist }
+    it { should be_enabled }
+  end
 end
 
-# Sensu API
-describe port(4567) do
-  it { should be_listening }
-  its('protocols') { should include 'tcp' }
-  its('addresses') { should be_in ['0.0.0.0', '::'] }
+if os.name == 'ubuntu'
+  describe apt('https://packagecloud.io/sensu/prerelease/ubuntu/') do
+    it { should exist }
+    it { should be_enabled }
+  end
 end
 
-# Sensu Client TCP/UDP Socket
-describe port(3030) do
-  it { should be_listening }
-  its('protocols') { should include 'tcp' }
-  # Broken on 14.04 - its('protocols') { should include 'udp' }
-  its('addresses') { should include '127.0.0.1' }
+if os.redhat?
+  describe package('pygpgme') do
+    it { should be_installed }
+  end
+
+  describe package('yum-utils') do
+    it { should be_installed }
+  end
+
+  describe yum.repo('sensu_prerelease') do
+    it { should exist }
+    it { should be_enabled }
+  end
+
+  describe yum.repo('sensu_prerelease-source') do
+    it { should exist }
+    it { should be_enabled }
+  end
 end
 
-# Sensu Client HTTP Socket
-describe port(3031) do
-  it { should be_listening }
-  its('protocols') { should include 'tcp' }
-  its('addresses') { should include '127.0.0.1' }
+describe package('sensu-backend') do
+  it { should be_installed }
 end
 
-# Uchiwa
-describe port(3000) do
-  it { should be_listening }
-  its('protocols') { should include 'tcp' }
-  its('addresses') { should be_in ['0.0.0.0', '::'] }
+describe package('sensu-agent') do
+  it { should be_installed }
 end
 
-# Ensure Sensu API has one consumer
-describe http('http://127.0.0.1:4567/health',
-              auth: { user: 'admin', pass: 'secret' },
-              params: { consumers: 1 }) do
-  its('status') { should eq 204 }
+describe package('sensu-cli') do
+  it { should be_installed }
+end
+
+describe file('/etc/default/sensu-agent') do
+  it { should_not exist }
+end
+
+describe file('/etc/sysconfig/sensu-agent') do
+  it { should_not exist }
+end
+
+describe file('/etc/default/sensu-backend') do
+  it { should_not exist }
+end
+
+describe file('/etc/sysconfig/sensu-backend') do
+  it { should_not exist }
+end
+
+describe file('/etc/sensu/') do
+  it { should be_directory }
+  its('owner') { should eq 'sensu' }
+  its('owner') { should eq 'sensu' }
+  its('mode') { should cmp '0750' }
+end
+
+describe file('/etc/sensu/backend.yml.example') do
+  it { should_not exist }
+end
+
+describe file('/etc/sensu/backend.yml') do
+  its('owner') { should eq 'sensu' }
+  its('owner') { should eq 'sensu' }
+  its('mode') { should cmp '0640' }
+end
+
+describe yaml('/etc/sensu/backend.yml') do
+  its('state-dir') { should eq '/var/lib/sensu' }
+end
+
+describe file('/etc/sensu/agent.yml.example') do
+  it { should_not exist }
+end
+
+describe file('/etc/sensu/agent.yml') do
+  its('owner') { should eq 'sensu' }
+  its('owner') { should eq 'sensu' }
+  its('mode') { should cmp '0640' }
+end
+
+describe yaml('/etc/sensu/agent.yml') do
+  its(['backend-url', 0]) { should eq 'ws://127.0.0.1:8081' }
+  its('cache-dir') { should eq '/var/cache/sensu/sensu-agent' }
+end
+
+describe service('sensu-backend') do
+  it { should be_enabled }
+  it { should be_installed }
+  it { should be_running }
+end
+
+describe service('sensu-agent') do
+  it { should be_enabled }
+  it { should be_installed }
+  it { should be_running }
 end
